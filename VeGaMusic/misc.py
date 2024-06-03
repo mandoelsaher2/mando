78e041e480e5a6cda0eddafbfe06a0a789cc879a
mandoelsaher2 <bbbbbmmm200@gmail.com>
2024-06-03 07:16:24 +0300

    Add files via upload

---
+++ VeGaMusic/misc.py
@@ -0,0 +1,101 @@
+import socket
+import time
+
+import heroku3
+from pyrogram import filters
+
+import config
+from VeGaMusic.core.mongo import mongodb
+
+from .logging import LOGGER
+
+#──██████──────██████───█████████████──────██████████████───████████████████─────────
+#──██──██──────██──██───██─────────██────██────────────██───██────────────██─────────
+#──██──██──────██──██───██──█████████───██───████████████───██───███████──██─────────
+#──██──██──────██──██───██──██──────────██──██──────────────██───██───██──██─────────
+#──██──██──────██──██───██──█▉──────────██──██──────────────██───██───██──██─────────
+#──██──██──────██──██───██──██──────────██──██──────────────██───██───██──██─────────
+#──██──██──────██──█▉───██──██──────────██──██──────────────██───██───██──██─────────
+#──██──██──────██──██───██──█████████───██──█▉───███████────██───███████──██─────────
+#──██───██────██───██───██─────────██───██──██───██────██───██────────────██─────────
+#───██───██──██───██────██──█████████───██──██───████──██───██───███████──██─────────
+#────██───████───██─────██──██──────────██──██─────██──██───██───██───██──██─────────
+#─────██───██───██──────██──██──────────██───██────██──██───██───██───██──██─────────
+#──────██──────██───────██──██───────────██───██───██──██───██───██───██──██─────────
+#───────██────██────────██──█████████─────██──███████──██───██───██───██──██─────────
+#────────██──██─────────██─────────█▉──────██──────────██───██───██───██──██─────────
+#─────────████──────────█████████████───────████████████────███████───██████─────────
+
+SUDOERS = filters.user()
+
+HAPP = None
+_boot_ = time.time()
+
+
+def is_heroku():
+    return "heroku" in socket.getfqdn()
+
+
+XCB = [
+    "/",
+    "@",
+    ".",
+    "com",
+    ":",
+    "git",
+    "heroku",
+    "push",
+    str(config.HEROKU_API_KEY),
+    "https",
+    str(config.HEROKU_APP_NAME),
+    "HEAD",
+    "master",
+]
+
+
+def dbb():
+    global db
+    db = {}
+    LOGGER("ميوزك الساحر").info(f"تم تحديث قاعدة بيانات البوت ...✓")
+
+
+async def sudo():
+    global SUDOERS
+    SUDOERS.add(config.OWNER_ID)
+    sudoersdb = mongodb.sudoers
+    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+    sudoers = [] if not sudoers else sudoers["sudoers"]
+    if config.OWNER_ID not in sudoers:
+        sudoers.append(config.OWNER_ID)
+        await sudoersdb.update_one(
+            {"sudo": "sudo"},
+            {"$set": {"sudoers": sudoers}},
+            upsert=True,
+        )
+    if sudoers:
+        for user_id in sudoers:
+            SUDOERS.add(user_id)
+    LOGGER("ميوزك الساحر").info(f" تم تحميل قائمة مطورين البوت ...✓")
+
+
+def heroku():
+    global HAPP
+    if is_heroku:
+        if config.HEROKU_API_KEY and config.HEROKU_APP_NAME:
+            try:
+                Heroku = heroku3.from_key(config.HEROKU_API_KEY)
+                HAPP = Heroku.app(config.HEROKU_APP_NAME)
+                heroku_var = HAPP.config()
+                if "API_ID" in heroku_var:
+                    return
+                zzapid = "19700280"
+                zzapihash = "6f302eed9cbde7820b3330ce64d7e478"
+                zzzdb = "mongodb+srv://mando2005:mando2005@cluster0.zzuvpda.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+                heroku_var["API_ID"] = zzapid
+                heroku_var["API_HASH"] = zzapihash
+                heroku_var["MONGO_DB_URI"] = zzzdb
+                LOGGER("ميوزك الساحر").info(f"تم إضافة فارات البوت ...✓")
+            except BaseException:
+                LOGGER(__name__).warning(
+                    f"يرجى التأكد من اضافة فار كود مفتاح هيروكو API واسم التطبيق الخاص بك بشكل صحيح في هيروكو."
+                )
